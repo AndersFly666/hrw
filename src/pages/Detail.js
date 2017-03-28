9a94c2188c95c6c186ecdb8ceb395b1c69b5acbb
AndersFly666 <andrewfly666@gmail.com>
2017-03-28 11:23:16 +0300

    Add state to component

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Chance from 'chance';
 
 class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: chance.first(),
+      country:  chance.country({ full: true })
+    }
+  }
+
   render() {
-    return <p>{ this.props.message }</p> 
+    return (
+      <div>
+        <p>Hello, { this.state.name }.</p>
+        <p>You are from { this.state.country }</p> 
+      </div>
+    )
   }
 }
 
