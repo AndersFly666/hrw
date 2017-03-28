b03496b2a44c0c7f60149ab53da3d912f0c41f68
AndersFly666 <andrewfly666@gmail.com>
2017-03-28 11:09:02 +0300

    Add props to component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import Detail from './pages/Detail';
 
 ReactDOM.render(
-  <Detail />,
+  <Detail message="Sample message from props!"/>,
   document.getElementById('app')
 );
